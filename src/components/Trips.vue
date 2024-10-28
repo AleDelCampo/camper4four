023c9d22577050c78f12689271f552ede54c0e13
AleHashirama <alehashirama@gmail.com>
2024-10-28 19:31:04 +0100

    first commit

---
+++ src/components/Trips.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Lista di Elementi</h1>
+        <ul>
+            <li v-for="trip in trips" :key="trip.id">
+                <h2>{{ trip.name }}</h2>
+                <p>{{ trip.date }}</p>
+                <img :src="`https://camper4fourdev-38684757747e.herokuapp.com/storage/${trip.image_path}`" alt="Image" width="200">
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            trips: []
+        };
+    },
+    mounted() {
+        this.fetchTrips();
+    },
+    methods: {
+        async fetchTrips() {
+            try {
+                const response = await axios.get('https://camper4fourdev-38684757747e.herokuapp.com/trips');
+                this.trips = response.data;
+            } catch (error) {
+                console.error("Si è verificato un errore nel caricamento degli elementi:", error);
+            }
+        }
+    }
+};
+</script>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Benvenuto nella Homepage</h1>
+        <Trips />
+    </div>
+</template>
+
+<script>
+import Trips from '../Trips.vue';
+
+export default {
+    components: {
+        Trips,
+    },
+};
+</script>
